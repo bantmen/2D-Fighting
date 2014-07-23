8e15015f703e7495e83cb3baf98eec2db045dcb4
cberkay <c.berkayantmen@gmail.com>
2014-07-23 01:23:51 -0400

    single, multi scenes and statusbar1.cs problem

---
+++ Assets/Scripts/StatusBar1AI.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StatusBar1AI : MonoBehaviour {
+
+	GameObject go;
+	PlayerMoves2AI script;
+
+	void Start () {
+		go = GameObject.Find ("2D Character-2-AI");  //FIX IT BACK FOR THE MULTIPLAYER
+		script = go.GetComponent <PlayerMoves2AI> ();
+
+	}
+
+	void FixedUpdate () {
+		//guiText.text = "Health: " + script.hitPoint;
+	}
+}
